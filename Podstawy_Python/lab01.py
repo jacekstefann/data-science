2d8c7c76e49e813232ff8162d5c3d1f99b1c79b7
Jacek Stefan <jacek.stefann@gmail.com>
2021-10-16 11:57:38 +0200

    pierwsze zajęcia - kod

--- Podstawy_Python/lab01.py
+++ Podstawy_Python/lab01.py
@@ -1,1 +1,17 @@
 print("hello World!")
+
+imie = "Jacek"
+nazwisko = "Stefan"
+
+# konkatenacja
+imie_nazwisko = imie + " " + nazwisko
+print(imie_nazwisko)
+
+# typ zmiennych
+print("Typ zmiennej imie_nazwisko to: ", type(imie_nazwisko))
+
+# zadania:
+liczba_calkowita = 8                #int
+liczba_zmiennoprzecinkowa = 4.5     #float
+
+
